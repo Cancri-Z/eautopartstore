5e457d7d55d5b38ac8788924a73c1444eca69fb4
Cancri-Z <badmosjohn2@gmail.com>
2024-08-27 16:40:03 +0100

    Some changes

--- public/javaScript_folder/listtoggle.js
+++ public/javaScript_folder/listtoggle.js
@@ -6,6 +6,10 @@
   const brdhide = document.getElementById('brdhide');
   const closeBrandsList = document.getElementById('cnl');
 
+  // Ensure initial state is set correctly
+  list.style.display = 'none';
+  brdhide.style.display = 'none';
+
   // Add click event listener to the toggleButton
   toggleButton.addEventListener('click', toggleListVisibility);
 
@@ -34,7 +38,7 @@
 
   // Function to toggle the visibility of brands
   function toggleBrandsVisibility() {
-    if (brdhide.style.display === '' || brdhide.style.display === 'none') {
+    if (brdhide.style.display === 'none') {
       brdhide.style.display = 'flex';
     } else {
       brdhide.style.display = 'none';
@@ -58,8 +62,4 @@
   });
 
 })();
- 
- 
-
-
 
